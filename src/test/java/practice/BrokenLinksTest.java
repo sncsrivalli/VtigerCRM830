9cb674dbd30fe0bf9101cc7c0142c1d04e714e44
QPS-Basavanagudi <QPS-Basavanagudi@DESKTOP-F52EDBA>
2023-03-26 15:51:08 +0530

    Comment added

--- src/test/java/practice/BrokenLinksTest.java
+++ src/test/java/practice/BrokenLinksTest.java
@@ -14,7 +14,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
+//Broken links
 public class BrokenLinksTest {
 
 	@Test
